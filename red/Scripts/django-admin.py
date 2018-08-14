e1a369345dad912b3f043579725dd93e6411df57
Anjan Tatavarthi <anjantatavarthi1997@gmail.com>
2018-08-14 23:36:16 +0530

    second commit

--- red/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Django projects\Redbus\red\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- venv/Lib/site-packages/django_inlinecss/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from django_inlinecss.tests.test_templatetags import *
